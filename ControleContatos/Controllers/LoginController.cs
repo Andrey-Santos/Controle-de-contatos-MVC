154c9da0156e4d834156e7abcfd5839d32f2aed0
Andey carlos dos santos <Andrey-Carlos@outlook.com>
2025-01-05 10:48:18 -0300

    Criado sessão de usuário

--- ControleContatos/Controllers/LoginController.cs
+++ ControleContatos/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using ControleContatos.Models;
+﻿using ControleContatos.Helper;
+using ControleContatos.Models;
 using ControleContatos.Repositorio;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,14 +8,28 @@ namespace ControleContatos.Controllers
     public class LoginController : Controller
     {
         private readonly IUsuarioRepositorio _usuarioRepositorio;
+        private readonly ISessao _sessao;
 
-        public LoginController(IUsuarioRepositorio usuarioRepositorio)
+        public LoginController(IUsuarioRepositorio usuarioRepositorio, ISessao sessao)
         {
             _usuarioRepositorio = usuarioRepositorio;
+            _sessao = sessao;
         }
         public IActionResult Index()
         {
+            //SE O USUÁRIO JÁ ESTIVER LOGADO, REDIRECIONA PARA A PÁGINA HOME
+
+            if (_sessao.BuscarSessaoUsuario() != null)
+                return RedirectToAction("Index", "Contato");
+
             return View();
+        }
+
+        public IActionResult Sair()
+        {
+            _sessao.DestruirSessaoUsuario();
+
+            return RedirectToAction("Index", "Login");
         }
 
         [HttpPost]
@@ -26,14 +41,17 @@ namespace ControleContatos.Controllers
                 {
                     UsuarioModel usuario = _usuarioRepositorio.BuscarPorLogin(loginModel.Login);
 
-                    if (usuario != null)
-                        if (usuario.SenhaValida(loginModel.Senha))
-                            return RedirectToAction("Index", "Contato");
+                    if (usuario != null && usuario.SenhaValida(loginModel.Senha))
+                    {
+                        _sessao.CriarSessaoUsuario(usuario);
+                        return RedirectToAction("Index", "Contato");
+                    }
 
                     TempData["MensagemErro"] = "Login ou senha inválidos. Por favor, tente novamente.";
 
                 }
-                    return View("Index", loginModel);
+
+                return View("Index", loginModel);
             }
             catch (Exception erro)
             {
--- ControleContatos/Program.cs
+++ ControleContatos/Program.cs
@@ -1,4 +1,5 @@
 using ControleContatos.Data;
+using ControleContatos.Helper;
 using ControleContatos.Repositorio;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,8 +11,17 @@ builder.Services.AddControllersWithViews();
 // Adds database context to dependency injection container
 builder.Services.AddDbContext<BancoContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DataBase")));
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddScoped<IContatoRepositorio, ContatoRepositorio>();
 builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+builder.Services.AddScoped<ISessao, Sessao>();
+
+builder.Services.AddSession(o =>
+{
+    o.Cookie.HttpOnly = true;
+    o.Cookie.IsEssential = true;
+});
 
 var app = builder.Build();
 
@@ -29,6 +39,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
